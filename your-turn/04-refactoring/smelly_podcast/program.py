666f2369fe1ea34c4bb126fe090c6dbe5d2f0b74
Chris Boendermaker <38512200+Pheadrus61@users.noreply.github.com>
2024-04-20 09:53:19 +1000

    Fixed smelly code

--- your-turn/04-refactoring/smelly_podcast/program.py
+++ your-turn/04-refactoring/smelly_podcast/program.py
@@ -1,50 +1,39 @@
-from collections import namedtuple
-from xml.etree import ElementTree
+from typing import Any
 
-import requests
-
-Episode = namedtuple('Episode', 'title link pubdate show_id')
-episode_data = {}
+from service import get_episode, get_show_id_range, download_data
 
 
 def main():
-    # SHOW THE HEADER
-    print("Welcome to the talk python info downloader.")
-    print()
+
+    show_header()
 
     # DOWNLOAD THE EPISODE DATA
-    url = 'https://talkpython.fm/episodes/rss'
-
-    resp = requests.get(url)
-    resp.raise_for_status()
-
-    dom = ElementTree.fromstring(resp.text)
-
-    episode_count = len(dom.findall('channel/item'))
-
-    for idx, item in enumerate(dom.findall('channel/item')):
-        episode = Episode(
-            item.find('title').text,
-            item.find('link').text,
-            item.find('pubDate').text,
-            episode_count - idx - 1
-        )
-        episode_data[episode.show_id] = episode
+    download_data('https://talkpython.fm/episodes/rss')
 
     # GET LATEST SHOW ID
-    latest_show_id = max(episode_data.keys())
-    oldest_show_id = min(episode_data.keys())
+    latest_show_id, oldest_show_id = get_show_id_range()
 
     print("Working with total of {} episodes".format(latest_show_id))
 
+    end: int | Any
+    end, start = display_results(latest_show_id, oldest_show_id)
+
+    for show_id in range(start, end):
+        info = get_episode(show_id)
+        print("{}. {}".format(info.show_id, info.title))
+
+
+def display_results(latest_show_id, oldest_show_id):
     # DISPLAY RESULTS
     start = oldest_show_id
     end = latest_show_id + 1
+    return end, start
 
-    for show_id in range(start, end):
-        # GET EPISODE
-        info = episode_data.get(show_id)
-        print("{}. {}".format(info.show_id, info.title))
+
+def show_header():
+    # SHOW THE HEADER
+    print("Welcome to the talk python info downloader.")
+    print()
 
 
 if __name__ == '__main__':
